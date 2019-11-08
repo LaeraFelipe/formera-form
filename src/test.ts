c683c5eb97196fcdd0ad10011bf64d9eb95a56f4
Felipe Laera <felipe.laera@santosistemas.com.br>
2019-11-08 13:41:27 -0300

    Utilizando isEqual simples.

--- src/test.ts
+++ src/test.ts
@@ -1,11 +1,8 @@
-import Formera from "./Formera";
-import clone from './utils/clone';
+
 
 import * as _ from 'lodash';
-import set from "./utils/set";
-import get from "./utils/get";
-
-const object = {};
+import { isEqual } from './utils';
+console.log('isEqual', isEqual)
 
 const initialValues = {
   name: 'NameTest',
@@ -38,19 +35,44 @@ const initialValues = {
   ]
 }
 
+const otherValues = {
+  name: 'NameTest',
+  lastName: 'LastNameTest',
+  gender: 'Male',
+  typeId: 1,
+  address: [
+    {
+      zipCode: '17045',
+      city: 'North Jeramyland',
+      street: 'Gerlach Mission',
+      numer: '120'
+    },
+    {
+      zipCode: '170442',
+      city: 'Nova York',
+      street: 'Test street',
+      numer: '500'
+    }
+  ],
+  phones: [
+    {
+      description: 'Telefone 1',
+      phone: '(99) 999999999'
+    },
+    {
+      description: 'Telefone 2',
+      phone: '(35) 999999999'
+    }
+  ]
+}
 
-console.time('MEU SET')
-set(initialValues, 'address[2].zipCode', 111);
-console.timeEnd('MEU SET');
+console.time('tes')
+console.log(isEqual(initialValues, otherValues));
+console.timeEnd('tes')
+
 console.time('LOD')
-_.set(initialValues, 'address[2].zipCode', 111);
-console.timeEnd('LOD');
-console.time('MEU get')
-const result = get(initialValues, 'address[1].zipCode');
-console.timeEnd('MEU get');
+console.log(_.isEqual(initialValues, otherValues));
+console.timeEnd('LOD')
 
-console.log('result :', result);
 
-console.time('LOD GET')
-_.get(initialValues, 'address[2].zipCode');
-console.timeEnd('LOD GET');
+
--- src/utils/isEqual.ts
+++ src/utils/isEqual.ts
@@ -1,17 +1,72 @@
 /**Return if a value is a object. */
-function isObject(value: any) {
+function isObject(value: any): boolean {
   return typeof value === 'object';
 }
 
-/**Return if two values is equals. */
-export default function isEqual(value: any, other: any) {
-  if (value === other) {
-    return true;
-  }
+/**Compare arrays. */
+function isArrayEqual(value: Array<any>, other: Array<any>): boolean {
+  const maxLength = Math.max(value.length, other.length);
 
-  if (value == null || other == null || (!isObject(value) && !isObject(other))) {
-    return false;
+  for (let index = 0; index < maxLength; index++) {
+    if (!isEqual(value[index], other[index])) {
+      return false;
+    }
   }
 
   return true;
 }
+
+/**Compare objects. */
+function isObjectEqual(value: any, other: any): boolean {
+  let keys = Object.keys(value);
+
+  Object.keys((otherKey: string) => keys.indexOf(otherKey) === -1 && keys.push(otherKey));
+
+  for (const key of keys) {
+    if (!isEqual(value[key], other[key])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+/**Return if two values is equals. */
+export default function isEqual(value: any, other: any): boolean {
+  return JSON.stringify(value) === JSON.stringify(other);
+}
+
+// export default function isEqual(value: any, other: any) {
+//   if (value === other) {
+//     return true;
+//   }
+
+//   if (value == null || other == null || (!isObject(value) && !isObject(other))) {
+//     return false;
+//   }
+
+//   const valueIsArray = Array.isArray(value);
+//   const otherIsArray = Array.isArray(other);
+
+//   if (valueIsArray !== otherIsArray) {
+//     return false;
+//   }
+
+//   //Compare arrays.
+//   if (valueIsArray && otherIsArray) {
+//     console.log('Comparando arrays')
+//     if (!isArrayEqual(value, other)) {
+//       return false;
+//     }
+//   }
+//   //Compare objects.
+//   else {
+//     console.log('Comparando object')
+//     if (!isObjectEqual(value, other)) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+
