88917a0d69b8b88d57d49d5d575b3613388b9ce1
Felipe Laera <laera.felipe@gmail.com>
2020-07-23 15:23:45 -0300

    child search improvement.

--- src/utils/field.ts
+++ src/utils/field.ts
@@ -1,9 +1,17 @@
+const FIELD_DELIMITER_CHARACTERS = ['[', '.'];
+
 /**Returns if a field name is child of the other. */
 export function isFieldChild(field: string, toCompare: string) {
   //If is root.
-  if ((field == null || field === '')) return true;
+  if (field == null || field === '') {
+    return true;
+  }
 
-  const regxp = new RegExp(`^${field}(\\[\\d+\\]\\.|\\.).*`);
+  if (toCompare.startsWith(field) && toCompare.length > (field.length + 1)) {
+    if (FIELD_DELIMITER_CHARACTERS.indexOf(toCompare[field.length]) > -1) {
+      return true;
+    }
+  }
 
-  return regxp.test(toCompare);
+  return false;
 }
